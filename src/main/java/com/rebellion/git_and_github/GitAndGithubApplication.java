48d0f018214b81522f902c3fb23f07366b454a3f
Rebellion <avinashee0012@gmail.com>
2026-01-18 12:30:31 +0530

    chore: initialize project structure

---
+++ src/main/java/com/rebellion/git_and_github/GitAndGithubApplication.java
@@ -0,0 +1,13 @@
+package com.rebellion.git_and_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAndGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAndGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebellion/git_and_github/GitAndGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebellion.git_and_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAndGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
