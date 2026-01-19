e0e182846a8f3efba3c332a0cb6004e75c489bb9
avinashee0012 <avinashee0012@gmail.com>
2026-01-19 18:13:32 +0530

    feat: add getAllTasks method in TaskController for listing tasks

--- src/main/java/com/rebellion/git_and_github/controller/TaskController.java
+++ src/main/java/com/rebellion/git_and_github/controller/TaskController.java
@@ -1,7 +1,10 @@
 package com.rebellion.git_and_github.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,10 @@ public class TaskController {
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody TaskRequestDto taskRequestDto){
         return ResponseEntity.status(HttpStatus.OK).body(taskService.createTask(taskRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(){
+        return ResponseEntity.status(HttpStatus.OK).body(taskService.getAllTasks());
     }
 }
